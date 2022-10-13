340d76d2d424c5b6a5abe99eadf3d733476d3d3f
Nozyx59 <105104866+Nozyx59@users.noreply.github.com>
2022-10-13 19:53:19 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+setTimeout(() => {
+    alert("Voilà !\nÇa fait une minute !\nJe voulait vous montrer ça\n(Pup le hérisson)")
+}, 60000);
+
+document.getElementById("hello_pup").onclick = function () { alert("Bonjour, c'est moi Pup le hérisson !\nLa mascotte de vos incroyables talents !\n⭐ ⭐ ⭐ ⭐ ⭐"); };
